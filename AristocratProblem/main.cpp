cec8606ceb72bb88a8d7397814869edfa38bff83
Matthew Skelley <matthewskelley@192-168-1-6.tpgi.com.au>
2017-01-19 11:01:36 +1100

    Initial Commit

---
+++ AristocratProblem/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AristocratProblem
+//
+//  Created by Matthew Skelley on 19/01/2017.
+//  Copyright © 2017 Matthew Skelley. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
